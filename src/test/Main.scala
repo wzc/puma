b2c646c19f8206565537dd20db744e615179671a
Harmony Singh <harmony.singh@yahoo.com>
2017-06-30 15:02:43 +0100

    Test 2

--- src/test/Main.scala
+++ src/test/Main.scala
@@ -3,11 +3,15 @@ import lang.LightAndroid
 import effect.Tiny
 
 object Main extends App {
-/*
+
   val work_dir = "/Users/weichen/exp/temp"
   val classes_dex_file = work_dir + "/classes.dex"
   val dex_dump_file = work_dir + "/dex_dump.txt"
   val la = new LightAndroid(classes_dex_file, dex_dump_file)
+  var bd = la.method.body("Lcom/app/demo/MainActivity;", "gcd", "(II)V")
+  bd.foreach(x => println(x.toString))
+  
+/*
   println(la.static_field.get("Ljava/lang/Object;"))
   println(la.static_field.get("Lcom/app/demo/MainActivity;"))
   println(la.interface.get("Lcom/app/demo/MainActivity;"))
@@ -15,7 +19,6 @@ object Main extends App {
   println(la.method.body("Lcom/app/demo/MainActivity;", "gcd", "(II)V").toString)
   println(la.method.regs("Lcom/app/demo/MainActivity;", "gcd", "(II)V").toString)
   println(la.method.body("Landroid/support/graphics/drawable/PathParser;", "copyOfRange", "([FII)[F").toString)
-
   val work_dir = "/Users/weichen/exp/temp"
   var prg_file = work_dir + "/loop.tiny"
   println(prg_file)
