3c49f3007788ea9e0715f6a78a1887b4dafb833c
Harmony Singh <harmony.singh@yahoo.com>
2017-07-06 15:55:02 +0100

    Implementing jmp and mov

--- src/lang/Lambda.scala
+++ src/lang/Lambda.scala
@@ -38,24 +38,40 @@ package lang
  * @author Wei Chen, Harmony Signh @ University of Edinburgh, 01/07/2017 - 07/07/2017 
  */
 
-class Lambda() {  
+ class Lambda() {  
 
   /** Expression. 
    * Exp ::= Const | Var | Fld | Fun [Exp] | Op [Exp] | Lab | Abs [Var] Exp | App Exp Exp | Let Exp Exp Exp | Cond [Exp] 
    */
-  /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
-  sealed abstract class Exp
-    case class Const (s:String) extends Exp {override def toString = s} 
-    case class Var (s:String) extends Exp {override def toString = s} 
-    case class Fld (s:String, f:String) extends Exp {override def toString = s + "." + f} 
-    case class Fun (s:String, xs:List[Exp]) extends Exp {override def toString = s + " " + xs.map(x => x.toString).reduceLeft(_+ " " +_)} 
-    case class Op (xs:List[Exp]) extends Exp {override def toString = "op " + xs.map(x => x.toString).reduceLeft(_+ " " +_)} 
-    case class Lab (i:Int) extends Exp {override def toString = i.toString} 
-    case class Abs (xs:List[Exp], e:Exp) extends Exp {override def toString = "\\" + xs.map(x => x.toString).reduceLeft(_+ " " +_) + "." + e} 
-    case class App (ea:Exp, eb:Exp) extends Exp {override def toString = ea + " " + eb} 
-    case class Let (ea:Exp, eb:Exp, ec:Exp) extends Exp {override def toString = "let " + ea + " = " + eb + " in " + ec} 
-    case class Cond (c:Exp, es:List[Exp]) extends Exp {override def toString = "cond(" + c + es.map(x => x.toString).reduceLeft(_+ " " +_) + ")"} 
-  /*--End-of-Expression--------------------------------------------------------------------------------------------------------------------------------------*/
-  val x = Let (Var ("y"), Op (List(Var ("x"), Var ("y"))), Const ("unit")) 
-  println(x)
+   /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
+   sealed abstract class Exp
+   case class Const (s:String) extends Exp {override def toString = s} 
+   case class Var (s:String) extends Exp {override def toString = s} 
+   case class Fld (s:String, f:String) extends Exp {override def toString = s + "." + f} 
+   case class Fun (s:String, xs:List[Exp]) extends Exp {override def toString = s + " " + xs.map(x => x.toString).reduceLeft(_+ " " +_)} 
+   case class Op (xs:List[Exp]) extends Exp {override def toString = "op " + xs.map(x => x.toString).reduceLeft(_+ " " +_)} 
+   case class Lab (i:Int) extends Exp {override def toString = i.toString} 
+   case class Abs (xs:List[Exp], e:Exp) extends Exp {override def toString = "\\" + xs.map(x => x.toString).reduceLeft(_+ " " +_) + "." + e} 
+   case class App (ea:Exp, eb:Exp) extends Exp {override def toString = ea + " " + eb} 
+   case class Let (ea:Exp, eb:Exp, ec:Exp) extends Exp {override def toString = "let " + ea + " = " + eb + " in " + ec} 
+   case class Cond (c:Exp, es:List[Exp]) extends Exp {override def toString = "cond(" + c + es.map(x => x.toString).reduceLeft(_+ " " +_) + ")"} 
+   /*--End-of-Expression--------------------------------------------------------------------------------------------------------------------------------------*/
+  
+
+  // Takes Ins as a parameter, which consists of an operator (String), targets (List[String]) and sources (List[String])
+  def f(op: String, tar: List[String], src: List[String]) : Exp = op match{
+
+    case "jmp"=> Abs(src.map(x=>Var(x)), Cond( src.map(x=>Var(x)), List( App(tar.map(x=>Lab(x.toInt+1)), src.map(x=>Var(x))), App(Lab(l.toInt+1), src.map(x=>Var(x))) )))
+
+    case "mov"=> Abs(src.map(x=>Var(x)), Let(Var(tar.head), Var(src.head), App(List(Lab(l.toInt+1)), src.map(x=>Var(x))) ))
+
+    /*case "op"=> 
+
+    case "ret"=> 
+
+    case "inv"=> */
+
+  }
+
+  println(f("jmp", List("12","13"), List("12","54")))
 }
