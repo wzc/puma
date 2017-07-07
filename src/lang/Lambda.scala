8b191c7156638abf1805c10efce9bf5914fff86e
wzc <ericwaynechen@gmail.com>
2017-07-07 15:13:47 +0100

    add ins2exp in Lambda

--- src/lang/Lambda.scala
+++ src/lang/Lambda.scala
@@ -38,26 +38,58 @@ package lang
  * @author Wei Chen, Harmony Signh @ University of Edinburgh, 01/07/2017 - 07/07/2017 
  */
 
- class Lambda() {  
+class Lambda(_la:LightAndroid) {
+
+  def la:LightAndroid = _la
 
   /** Expression. 
    * Exp ::= Const | Var | Fld | Fun [Exp] | Op [Exp] | Lab | Abs [Var] Exp | App Exp Exp | Let Exp Exp Exp | Cond [Exp] 
    */
-   /*---------------------------------------------------------------------------------------------------------------------------------------------------------*/
-   sealed abstract class Exp
-   case class Const (s:String) extends Exp {override def toString = s} 
-   case class Var (s:String) extends Exp {override def toString = s} 
-   case class Fld (s:String, f:String) extends Exp {override def toString = s + "." + f} 
-   case class Fun (s:String, xs:List[Exp]) extends Exp {override def toString = s + " " + xs.map(x => x.toString).reduceLeft(_+ " " +_)} 
-   case class Op (xs:List[Exp]) extends Exp {override def toString = "op " + xs.map(x => x.toString).reduceLeft(_+ " " +_)} 
-   case class Lab (i:Int) extends Exp {override def toString = i.toString} 
-   case class Abs (xs:List[Exp], e:Exp) extends Exp {override def toString = "\\" + xs.map(x => x.toString).reduceLeft(_+ " " +_) + "." + e} 
-   case class App (ea:Exp, eb:Exp) extends Exp {override def toString = ea + " " + eb} 
-   case class Let (ea:Exp, eb:Exp, ec:Exp) extends Exp {override def toString = "let " + ea + " = " + eb + " in " + ec} 
-   case class Cond (c:Exp, es:List[Exp]) extends Exp {override def toString = "cond(" + c + es.map(x => x.toString).reduceLeft(_+ " " +_) + ")"} 
-   /*--End-of-Expression--------------------------------------------------------------------------------------------------------------------------------------*/
-  
+  sealed abstract class Exp
+    case class Const (s:String) extends Exp {override def toString = s} 
+    case class Var (s:String) extends Exp {override def toString = s} 
+    case class Fld (s:String, f:String) extends Exp {override def toString = s + "." + f} 
+    case class Fun (s:String, xs:List[Exp]) extends Exp {override def toString = s + " " + xs.map(x => x.toString).foldLeft("")(_+ " " +_)} 
+    case class Op (xs:List[Exp]) extends Exp {override def toString = "op" + xs.map(x => x.toString).foldLeft("")(_+ " " +_)} 
+    case class Lab (i:Int) extends Exp {override def toString = i.toString} 
+    case class Abs (xs:List[Exp], e:Exp) extends Exp {override def toString = "\\" + xs.map(x => x.toString).foldLeft("")(_+ " " +_) + " . " + e} 
+    case class App (ea:Exp, xs:List[Exp]) extends Exp {override def toString = ea + xs.map(x => x.toString).foldLeft("")(_+ " " +_)} 
+    case class Let (ea:Exp, eb:Exp, ec:Exp) extends Exp {override def toString = "let " + ea + " = " + eb + " in " + ec} 
+    case class Cond (c:Exp, es:List[Exp]) extends Exp {override def toString = "cond(" + c  + es.map(x => x.toString).foldLeft("")(_+ ", " +_) + ")"}
+    case class Unit () extends Exp {override def toString = "unit"}
+    case class Star () extends Exp {override def toString = "*"}
 
+  type Label = String
+
+  def ins2exp(l:Label, ins:Ins, xs:List[Exp]) : (Label, Exp) = {
+    ins.op match {
+      case "jmp" => if (ins.src.length == 0) // unconditional jump 
+                      (l, Abs(xs, Cond(Star(), ins.ta.map(x => App(Lab(x.toInt), xs)))))
+                    else if (ins.ta.length == 1) // conditional jump 
+                      (l, Abs(xs, Cond(Op(ins.src.map(x => Var(x))), 
+                                          App(Lab(l.toInt + 1), xs) :: ins.ta.map(x => App(Lab(x.toInt), xs)))))  
+                    else // switches
+                      (l, Abs(xs, Cond(Op(ins.src.map(x => Var(x))), 
+                                          ins.ta.map(x => App(Lab(x.toInt), xs)))))  
+      case "mov" => Abs 
+      case _ => (l, Unit())
+    }   
+  }
+    
+
+  val bd = la.method.body("Lcom/app/demo/MainActivity;", "gcd", "(II)V")
+  val xs = 
+    la.method.args("Lcom/app/demo/MainActivity;", "gcd", "(II)V") match {
+      case Some(lst) => lst.map(x => Var(x))  
+      case None => List() 
+    }
+  bd match {
+    case Some(lst) => lst.foreach(x => println(x + "$$" + (ins2exp(x._1, x._2, xs))))
+    case None =>
+  }
+ 
+  
+/*
   // Takes Ins as a parameter, which consists of an operator (String), targets (List[String]) and sources (List[String])
   def f(op: String, tar: List[String], src: List[String]) : Exp = op match{
 
@@ -76,6 +108,6 @@ package lang
     }
 
     case "inv"=> Abs(xs, Let(Var(tar.head), App(src.map(x=>Var(x)), xs), App(Lab(l.toInt+1), xs) ))
-
   }
+   */
 }
