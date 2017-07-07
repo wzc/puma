7f6af6bb15e84493b7398d59aed6d6addff40f37
wzc <ericwaynechen@gmail.com>
2017-07-07 15:14:44 +0100

    testing new Lambda

--- src/test/Main.scala
+++ src/test/Main.scala
@@ -6,13 +6,11 @@ import effect.Tiny
 
 object Main extends App {
 
-  /*val work_dir = "/Users/harmony/exp/temp"
+  val work_dir = "/Users/weichen/exp/temp"
   val classes_dex_file = work_dir + "/classes.dex"
   val dex_dump_file = work_dir + "/dex_dump.txt"
   val la = new LightAndroid(classes_dex_file, dex_dump_file)
-  val bd = la.method.body("Lcom/app/demo/MainActivity;", "gcd", "(II)V")*/
-  
-  val x = new Lambda()
+  new Lambda(la)
 
 
 
